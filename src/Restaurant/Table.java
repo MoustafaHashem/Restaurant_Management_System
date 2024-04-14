0a5e2110b6d33033f280862873fc33ed2152ccb4
Mayar0311 <mayarwalid22@gmail.com>
2024-04-14 18:54:09 +0200

    Table class is made

--- src/Restaurant/Table.java
+++ src/Restaurant/Table.java
@@ -1,4 +1,38 @@
 package Restaurant;
 
+import Services.Order;
+import Services.Reservation;
+
 public class Table {
+    private int tableNum=0;
+    private boolean isReserved;
+    private Order order;
+    private Reservation reservation;
+
+    public Table(){
+        this.tableNum ++;
+        this.isReserved = false;
+    }
+
+    public int getTableNum() {
+        return tableNum;
+    }
+    public boolean isReserverd (){
+        return isReserved;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+    public void addReservation (Reservation reservation){
+        this.reservation = reservation;
+        isReserved = true;
+    }
+    public void addOrder (Order order){
+        this.order = order;
+    }
 }
