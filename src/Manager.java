fcbe58677e119124180f7d99dda555d0d411c017
MoustafaHashem <moustafahashem7770@gmail.com>
2024-04-14 14:53:50 +0200

    micheal task v2

--- src/Manager.java
+++ src/Manager.java
@@ -1,12 +1,42 @@
 
 public class Manager extends Employee{
-Restaurant my_Restaurant;
-public void addMenu(Menu m){
+    Restaurant my_Restaurant;
 
-}
-
-    @Override
-    public void print() {
-
+    public Manager(String name, int age, String address, int phone, int employeeID, String email, String password, int salary, String rank, Restaurant my_Restaurant) {
+        super(name, age, address, phone, employeeID, email, password, salary, rank);
+        this.my_Restaurant = my_Restaurant;
     }
+
+    public void addMenu(Menu m){
+        my_Restaurant.getMenus().add(m);
+    }
+
+    public void removeMenu(Menu m){
+        my_Restaurant.getMenus().remove(m);
+    }
+    public void addMenuSection(MenuSection ms){
+        my_Restaurant.getMenuSections().add(ms);
+    }
+    public void removeMenuSection(MenuSection ms){
+        my_Restaurant.getMenuSections().remove(ms);
+    }
+    public void addMenuIteme(MenuIteme mi){
+        my_Restaurant.getMenuItemes().add(mi);
+    }
+    public void removeMenuIteme(MenuIteme mi){
+        my_Restaurant.getMenuSections().remove(mi);
+    }
+    public void addTable(Table t){
+        my_Restaurant.getTables().add(t);
+    }
+    public void removeTable(Table t){
+        my_Restaurant.getTables().remove(t);
+    }
+    public void addEmployee(Employee e){
+        my_Restaurant.getEmployees().add(e);
+    }
+    public void removeEmployee(Employee e){
+        my_Restaurant.getTables().remove(e);
+    }
+
 }
