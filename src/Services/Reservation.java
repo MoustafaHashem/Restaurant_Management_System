391d10c774240ae485ff55bf608ff5bac1cd010e
karen <karenmourice@gmail.com>
2024-04-16 17:35:22 +0200

    4/16 2:07 modified by karen

--- src/Services/Reservation.java
+++ src/Services/Reservation.java
@@ -52,9 +52,10 @@ private final static ArrayList<Reservation> reservations=new ArrayList<>();
         int size = reservations.size();
         int i;
         boolean invalidReservation = true;
-        int id= input.nextInt();
+
         do {
             System.out.println("Enter ID of Reservation to be cancelled: ");
+            int id= input.nextInt();
         for (i = 0; i < size; i++) {
             if (reservations.get(i).getReservationId() == id)
             {
@@ -110,72 +111,7 @@ private final static ArrayList<Reservation> reservations=new ArrayList<>();
         reservations.get(i).setDate(d);
         System.out.println("changed");
     }
-    public  static void makeReservation(LocalDate d, int n){
-        Scanner in=new Scanner(System.in);
 
-        int size = Manager.getTables().size();
-        int i;
-        ArrayList <Table> availableTables = new ArrayList<>();
-        int wantedTable;
-        boolean invalidTable=true;
-        for (i = 0; i < size; i++) {
-                if(!Manager.getTables().get(i).isReserved())
-                {
-                    availableTables.add(Manager.getTables().get(i));
-                }
-            }
-        if (!availableTables.isEmpty())
-        {
-            System.out.println("Let's start making your reservation :)");
-            System.out.println("Enter name: ");
-            String name=in.nextLine();
-            System.out.println("Enter age: ");
-            int age=in.nextInt();
-            System.out.println("Enter address: ");
-            String address=in.next();
-            System.out.println("Enter phone: ");
-            String phone=in.next();
-            Customer c = new Customer(name,age,address,phone);
-            Reservation r = new Reservation(d,n);
-            reservations.add(r);
-            System.out.println("Reservation ID:  "+r.getReservationId());
-            System.out.println("************************************************************");
-            System.out.println("Available tables for making a reservation: ");
-            System.out.println();
-            for(Table table:availableTables)
-            {
-                System.out.println("Table "+ table.getTableNum());
-            }
-            do {
-                System.out.println("\nWhich table do you want to make your reservation?");
-                wantedTable = in.nextInt();
-                for (Table table:availableTables) {
-                    if (table.getTableNum() == wantedTable) {
-                        invalidTable = false;
-                        break;
-                    }
-                }
-                if(!invalidTable)
-                {
-                    Manager.getTables().get(wantedTable).addReservation(r);
-                    c.checkIn();
-                    Manager.getTables().get(wantedTable).setCustomer(c);
-                    Manager.getTables().get(wantedTable).setIsReserved(true);
-                    System.out.println("Reservation done successfully");
-                    System.out.println("************************************************************");
-                }
-                else {
-                    System.out.println("Please enter a valid table number from the available tables listed above");
-                }
-            }while(invalidTable);
-
-        }
-        else
-        {
-            System.out.println("No available tables for making a reservation at the restaurant at the moment");
-            System.out.println("************************************************************");
-        }
-    }
     public static void printReservation()
     {
         for(Reservation r: reservations)
